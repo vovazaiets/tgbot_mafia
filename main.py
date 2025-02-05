713db8bf5728c501f53a338f881e9c4d43f34faf
vovazaietss <v.zts2005@gmail.com>
2025-02-06 00:07:22 +0200

    add func add bot to groups

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import telebot
 from dotenv import load_dotenv
 import os
-from telebot import InlineKeyboardMarkup,InlineKeyboardButton
+from telebot import types
 
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
+bot_username= os.getenv("username")
+add_bot_url = f"https://t.me/{bot_username}?startgroup=true"
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message,"hi this is mafia bot")
+    markup = types.InlineKeyboardMarkup()
+    add_to_group_button = types.InlineKeyboardButton("🤖Додати бота в свій чат",callback_data="addgroup",url=add_bot_url)
+    markup.add(add_to_group_button)
+    bot.reply_to(message,"hi this is mafia bot",reply_markup=markup)
 
 
 @bot.message_handler(func=lambda message:True)
@@ -19,6 +24,11 @@ def echo_message(message):
     bot.reply_to(message,"не викупив")
 
 
+# @bot.callback_querry_handler(func=lambda call:True)
+# def callback_querry(call):
+    # if call.data == 'addgroup':
+
+        
 
 
 bot.infinity_polling()
