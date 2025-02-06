7e2ead2e9ee7f7064e995fae4e441c68fe6e22b0
vovazaietss <v.zts2005@gmail.com>
2025-02-07 00:17:19 +0200

    add gif/add timer

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 import os
 from telebot import types
 import time
-
+import threading
+from types import SimpleNamespace
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -13,6 +14,9 @@ add_bot_url = f"https://t.me/{bot_username}?startgroup=true"
 bot = telebot.TeleBot(TOKEN)
 list_users = []
 
+REGESTRATION = False
+GAME_STARTED = False
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     if message.chat.type == 'private':
@@ -21,6 +25,7 @@ def send_welcome(message):
         markup.add(add_to_group_button)
         bot.reply_to(message,"hi this is mafia bot",reply_markup=markup)
     elif message.chat.type in ["group","supergroup"]:
+        print(GAME_STARTED)
         bot.reply_to(message,"Привіт я бот для гри в мафію🤖")
 
 
@@ -29,13 +34,12 @@ def update_message(message,first_name):
     message += f"\n{first_name}"
     return message
 
+# def send_start(message):
+#     # print("func work")
+#     bot.send_message(message.chat.id,"/startgame")
+# # messageRegestration = f"Починається реєстрація на гру\n"
 
 messageRegestration = f"Починається реєстрація на гру\n"
-
-list_players_markup = types.InlineKeyboardMarkup()
-
-REGESTRATION = False
-GAME_STARTED = False
 
 @bot.message_handler(commands=['game'])
 def start_regestration(message):
@@ -43,55 +47,84 @@ def start_regestration(message):
     if REGESTRATION == False:
         if message.chat.type in ["group","supergroup"]:
             global list_players_markup
+            global messageRegestration
+            messageRegestration = f"Починається реєстрація на гру\n"
+            GAME_STARTED = False
+            print(GAME_STARTED)
+            
             REGESTRATION = True
-            # list_players_markup = types.InlineKeyboardMarkup()
+            list_players_markup = types.InlineKeyboardMarkup()
             list_players_add = types.InlineKeyboardButton("Connect",callback_data="connectgame")
             list_players_markup.add(list_players_add)
             bot.send_message(message.chat.id,messageRegestration,reply_markup=list_players_markup)
+            timer_reg = threading.Timer(5.0,lambda: start_game(message))
+            timer_reg.start()
             bot.send_message(message.chat.id,"До кінця реєстрації залишилось: <time>")
         else:
             bot.reply_to(message,"this is command use in groups")
     elif REGESTRATION == True:
-        bot.send_message(message.chat.id,"Regestration started")
-
-
-
-@bot.callback_query_handler(func=lambda call:True)
-def callback_querry(call):
-    if call.data == 'connectgame':
-        REGESTRATION = True
-        print(call.from_user.first_name)
-        print(call.from_user.id)
-        id_user = call.from_user.id
-        firstname = call.from_user.first_name
-        bot.send_message(id_user,"You join to game")
-        if firstname in list_users:
-            bot.send_message(id_user,"no no no chill bro,you in game")
-        else:
-            list_users.append(firstname)
-            global list_players_markup
-            global messageRegestration
-            messageRegestration = update_message(message=messageRegestration,first_name=firstname)
-            bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text=messageRegestration,reply_markup=list_players_markup)
-            print(list_users[0])
-       
+        bot.delete_message(message.chat.id, message.message_id)
 
 @bot.message_handler(commands=['startgame'])
 def start_game(message):
     global REGESTRATION
+    global GAME_STARTED
     if REGESTRATION == True:
         REGESTRATION = False
         GAME_STARTED = True
+        print(GAME_STARTED)
         bot.send_message(message.chat.id, "Гра розпочалась")
+        with open("night01.mp4", "rb") as video:
+            bot.send_video(message.chat.id, video,timeout=60,caption="🌃 Настає ніч")
+
     else:
-        bot.reply_to(message, ":)")
+        bot.delete_message(message.chat.id, message.message_id)
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback_querry(call):
+    print(GAME_STARTED)
+    if call.data == 'connectgame':
+        if GAME_STARTED == False:
+            # REGESTRATION = True
+            print(call.from_user.first_name)
+            print(call.from_user.id)
+            id_user = call.from_user.id
+            firstname = call.from_user.first_name
+            bot.send_message(id_user,"You join to game")
+            if firstname in list_users:
+                bot.send_message(id_user,"no no no chill bro,you in game")
+            else:
+                list_users.append(firstname)
+                global list_players_markup
+                global messageRegestration
+                messageRegestration = update_message(message=messageRegestration,first_name=firstname)
+                bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text=messageRegestration,reply_markup=list_players_markup)
+                print(list_users[0])
+        elif GAME_STARTED == True:
+            print("pyk pyk")
+       
+
+
 
 
 @bot.message_handler(commands=['stop'])
 def stop_game(message):
-    REGESTRATION = False
-    GAME_STARTED = False
-    bot.send_message(message.chat.id, "Гру скасовано")
+    global REGESTRATION
+    if REGESTRATION == True:
+        messageRegestration = f"Починається реєстрація на гру\n"
+        list_users.clear()
+        REGESTRATION = False
+        list_players_markup = None
+        bot.send_message(message.chat.id,"Registation canceled")
+    elif GAME_STARTED == False:
+        messageRegestration = f"Починається реєстрація на гру\n"
+        list_users.clear()
+        bot.delete_message(message.chat.id, message.message_id)
+    elif GAME_STARTED == True:
+        messageRegestration = f"Починається реєстрація на гру\n"
+        list_users.clear()
+        GAME_STARTED == False
+        bot.send_message(message.chat.id, "Game canceled")
 
 # @bot.message_handler(func=lambda message:True)
 # def echo_message(message):
